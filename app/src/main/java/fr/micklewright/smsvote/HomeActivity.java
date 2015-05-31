3d301c54aee5d947800f57b0b5fd4dcfdadce78e
Alexander Micklewright <alexander.micklewright@student.ecp.fr>
2015-05-31 19:25:41 +0200

    Begin interface

--- app/src/main/java/fr/micklewright/smsvote/HomeActivity.java
+++ app/src/main/java/fr/micklewright/smsvote/HomeActivity.java
@@ -29,7 +29,7 @@ public class HomeActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.home_action_settings) {
             return true;
         }
 
